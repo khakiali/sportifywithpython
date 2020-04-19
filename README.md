ae9a1c3f1b3b18596a24af18bcc4f2b4188d2fda
AliKhaki <khakiali@hotmail.com>
2020-04-19 12:05:22 -0700

    Creation of project files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sportifywithpython
+# Project to take liked videos on youtube and create a spotify playlist
+
+# API's required:
+    # Youtube Data API
+    # Spotify Web API
+        User ID and OAuth token
+        add to secrets file
+    # YouTube DL
---
+++ main.py
@@ -0,0 +1,25 @@
+#Create Spotify playlist from Youtube likes
+
+class CreatePlayList:
+    def  _init_(self):
+        pass
+
+    # Step 1 Log into YouTube
+    def get_youtube_client(self):
+        pass
+
+    # Step 2 Grab our liked videos
+    def get_liked_videos(self):
+        pass
+
+    #Step 3 Create a new playlist
+    def create_playlist(self):
+        pass
+
+    #step 4 search for the song
+    def get_spotify_uri(self):
+        pass
+
+    #step 5 add song to the newly created playlist
+    def add_song_to_playlist(self):
+        pass
